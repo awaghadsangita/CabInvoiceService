0c936bbc2e7aaa5b7e2379429b8a6d106a398bd1
singhprince0088 <singhprince0088@gmail.com>
2019-12-20 12:01:40 +0530

    REFACTORED:rename variables

--- src/main/java/cabinvoicegenerator/RideTypeEnum.java
+++ src/main/java/cabinvoicegenerator/RideTypeEnum.java
@@ -9,9 +9,9 @@ public enum RideTypeEnum {
     public int COST_PER_TIME;
     public double MINIMUM_FARE;
 
-    RideTypeEnum(double MINIMUM_COST_PER_KILOMETER, int _COST_PER_TIME, double MINIMUM_FARE) {
+    RideTypeEnum(double MINIMUM_COST_PER_KILOMETER, int COST_PER_TIME, double MINIMUM_FARE) {
         this.MINIMUM_COST_PER_KILOMETER = MINIMUM_COST_PER_KILOMETER;
-        this.COST_PER_TIME = _COST_PER_TIME;
+        this.COST_PER_TIME = COST_PER_TIME;
         this.MINIMUM_FARE = MINIMUM_FARE;
     }
 }
